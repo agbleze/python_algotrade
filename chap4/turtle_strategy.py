5d15c333dd2d835d63485447f76cf57b248aedcb
agbleze <lagbleze@st.ug.edu.gh>
2023-03-18 15:19:07 +0100

    Create turtle_strategy.py

---
+++ chap4/turtle_strategy.py
@@ -0,0 +1,25 @@
+### turtle strategy
+# we are going to create a long signal when 
+# the price reaches the highest price for the last window_size days
+# We will create a short signal when the price reaches its lowest point. 
+# We will get out of a position by having the price crossing 
+# the moving average of the last window_size days
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
